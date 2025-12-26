3f0fcd74f9dc17bf79c781a17631b9ea856957c6
Michael <csusoftware0702@gmail.com>
2025-12-26 21:59:38 +0800

    complete ui service

--- ui/src/main/java/com/michaelzhu/exchange/UIApplication.java
+++ ui/src/main/java/com/michaelzhu/exchange/UIApplication.java
@@ -1,11 +1,35 @@
 package com.michaelzhu.exchange;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.michaelzhu.exchange.client.RestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UIApplication {
     public static void main(String[] args) {
         SpringApplication.run(UIApplication.class, args);
+    }
+
+    @Bean
+    public RestClient createRestClient(
+            @Value("#{exchangeConfiguration.apiEndpoints.tradingApi}") String tradingApiEndpoint,
+            @Autowired ObjectMapper objectMapper
+    ) {
+        return new RestClient.Builder(tradingApiEndpoint).build(objectMapper);
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+            }
+        };
     }
 }
