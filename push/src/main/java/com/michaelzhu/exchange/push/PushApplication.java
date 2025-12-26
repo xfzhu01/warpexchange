2f464c614ab40a2f7d90325d62cbac121c718947
Michael <csusoftware0702@gmail.com>
2025-12-26 21:21:06 +0800

    complete push service

--- push/src/main/java/com/michaelzhu/exchange/push/PushApplication.java
+++ push/src/main/java/com/michaelzhu/exchange/push/PushApplication.java
@@ -1,11 +1,21 @@
 package com.michaelzhu.exchange.push;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class) // disable db autoconfig
 public class PushApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PushApplication.class, args);
+        System.setProperty("vertx.disableFileCPResolving", "true");
+        System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
+
+        SpringApplication app = new SpringApplication(PushApplication.class);
+        // disable spring web
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 }
